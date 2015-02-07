cbeeb850d0687309de53137990446bcea2863faa
yu2 <tuyu0515@gmail.com>
2015-02-07 10:57:37 -0600

    Update tst.js

--- tst.js
+++ tst.js
@@ -1,1 +1,12 @@
-placeholder
+function myFunction() {
+  var resp = prompt("Rename");
+  document.getElementById("description").innerHTML = resp;
+  document.getElementById("title").innerHTML = resp;
+}
+      
+var x = 0;
+      
+function counter() {
+  x++;
+  document.getElementById("description").innerHTML = x;
+}
