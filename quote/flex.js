d8e142e80c0d8ac24809c70e757d55cd9d98fd2b
yu2 <tuyu0515@gmail.com>
2017-08-15 17:06:43 -0600

    Add files via upload

--- quote/flex.js
+++ quote/flex.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
   $(".item").mouseover(function() {
-    
+    $(".item").animate( {
+      width: "+= 20%",
+      height: "+= 20%"
+    });
+  });
+  $(".item").mouseleave(function() {
+    $(".item").animate( {
+      width: "-= 20%",
+      height: "-= 20%"
+    });
   });
 });
