ceb10d19c68bb95444aff8f26551ed17b45929a1
yu2 <tuyu0515@gmail.com>
2017-08-17 00:54:04 -0600

    Add files via upload

--- quote/flex.js
+++ quote/flex.js
@@ -11,17 +11,27 @@ $(document).ready(function() {
       height: "-= 20%"
     });
   });
+
+/*
   $(function(){
      alert("My First Jquery Test");
   });
+*/
 
   function modifyText() {
     var p = document.getElementById("text");
-    p.firstChild.nodeValue = "The element has been clicked.";
+    if (p.firstChild.nodeValue == "The element has been clicked.") {
+      p.firstChild.nodeValue = "This is some output.";
+    } else {
+      p.firstChild.nodeValue = "The element has been clicked.";
+    }
   }
   
-  var e = document.getElementById("text");
-  e.addEventListener("click", modifyText, false);
+  var e = document.getElementsByClassName("item");
+  for (var i = 0; i < e.length; i++) {
+    e[i].addEventListener("click", modifyText, false);  
+  }
+  
   
 
   
