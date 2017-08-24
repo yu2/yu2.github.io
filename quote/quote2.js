068089edc582f2edf7660185ef2da6f2aadc710d
yu2 <tuyu0515@gmail.com>
2017-08-23 22:21:34 -0600

    Add files via upload

--- quote/quote2.js
+++ quote/quote2.js
@@ -1,13 +1,41 @@
 $(document).ready(function() {
   var e = document.getElementById("one-more");
   
-  e.addEventListener("click", onClick(), false);
+  e.addEventListener("click", onClick, false);
   
-  console.log("yes");
+  var out_cont1 = document.getElementById("out-cont1");
+  var cont1 = document.getElementById("cont1");
+  var out_cont2 = document.getElementById("out-cont2");
+  var cont2 = document.getElementById("cont2");
   
   function onClick() {
-    $(".container").animate( {
-      width: "+=20%"
-    });
+    console.log($("#cont2").css("transform"));
+    
+    if($("#cont2").css("transform") == "matrix(-1, -1.22465e-16, 1.22465e-16, -1, 0, 0))") {
+      out_cont1.style.transform += "translateX(-65vw)";
+      cont1.style.transform += "rotate(-720deg)";
+      
+      console.log("72");
+      
+      out_cont2.style.transform += "translateX(-65vw)";
+      cont2.style.transform += "rotate(-720deg)";
+    } else {
+      out_cont1.style.transform += "translateX(-65vw)";
+      cont1.style.transform += "rotate(-360deg)";
+      
+      console.log("36");
+      console.log($("#cont2").css('transform'));
+      
+      out_cont2.style.transform += "translateX(-65vw)";
+      cont2.style.transform += "rotate(-360deg)";
+    }
+    
   }
+  /*
+  cont2.addEventListener("transitionend", function(event) {
+    $("#cont2").css("transform", "rotate(0)");
+  });
+  */
+  
+  
 });
