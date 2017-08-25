dd68bd5d832971e6615b224e54c753128286fbd6
yu2 <tuyu0515@gmail.com>
2017-08-25 12:01:04 -0600

    Add files via upload

---
+++ quote/anim.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  var t1 = document.getElementById("t1");
+  var listener = document.getElementById("listener");
+  console.log("I'm running")
+  t1.addEventListener("animationstart", function() {
+    listener.innerHTML = "Animation start.";
+  });
+  
+  t1.addEventListener("animationiteration", function() {
+    listener.innerHTML = "New iteration.";
+  });
+  
+  t1.addEventListener("animationend", function() {
+    listener.innerHTML = "Animation end.";
+    setTimeout(function(){listener.innerHTML = "Here's what's going on."}, 2000);
+  });
+});
