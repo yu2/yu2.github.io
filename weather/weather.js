24f171e244a0ab7de834b575ef8f662399cfb5bb
yu2 <tuyu0515@gmail.com>
2017-09-07 23:58:09 -0600

    Add files via upload

--- weather/weather.js
+++ weather/weather.js
@@ -2,21 +2,25 @@ var url = "https://fcc-weather-api.glitch.me/api/current?";
 var lat, lon;
 
 $(function() {
+  
   if (navigator.geolocation) {
-    alert("Geolocation available");
+    navigator.geolocation.getCurrentPosition(function(position) {
+      lat = position.coords.latitude;
+      lon = position.coords.longitude;
+      
+      $.ajax({
+        url: url + "lat=" + lat + "&lon=" + lon,
+        success: function(result) {
+          $("#temperature").html(result.weather[0].description);
+          $("#location").html(result.name);
+          $("#icon").html("<img src=\"" + result.weather[0].icon + "\" />");
+        }
+      });
+    });
   } else {
-    alert("Geolocation unavailable. Please refresh page and allow location sharing.")
+    alert("Geolocation unavailable. Please refresh page and allow location sharing.");
   }
-  
-  navigator.geolocation.getCurrentPosition(function(position) {
-    lat = position.coords.latitude;
-    lon = position.coords.longitude;
-  });
-  
- /* 
-  $.ajax({
-    url: url + ,
-    
-  })
-  */
+
+ 
+ 
 });
