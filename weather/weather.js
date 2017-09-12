37fe0706c8a6eefd10b6f1d57752f1b5339a0597
yu2 <tuyu0515@gmail.com>
2017-09-12 00:03:33 -0600

    Add files via upload

--- weather/weather.js
+++ weather/weather.js
@@ -1,8 +1,8 @@
 var url = "https://fcc-weather-api.glitch.me/api/current?";
 var lat, lon, icon;
+var res;
 
-$(function() {
-  
+$(document).ready(function() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
       lat = position.coords.latitude;
@@ -11,24 +11,51 @@ $(function() {
       $.ajax({
         url: url + "lat=" + lat + "&lon=" + lon,
         success: function(result) {
+          
+          res = result;
           var temp = result.weather[0].main;
           $("#condition").html(temp);
           $("#location").html(result.name);
           $("#icon").html(getIcon(temp));
+          bind(".inner-container");
           $("#temperature").html(result.main.temp);
           $("img").css("height", "5em");
           $(".inner-container").animate({
             opacity: 1
           }, 1000);
-          $(".container").animate({
-            transform: "rotate(180deg)"
-          });
+          
         }
       });
     });
-  } else {
+  }
+  else {
     alert("Geolocation unavailable. Please refresh page and allow location sharing.");
   }
+  
+  function bind(target) {
+  
+  $(document).on("click", target, function() {
+    alert("clicked");
+  });
+  
+  }
+  
+  $("#cel").click(function(){
+    $("#temperature").html(res.main.temp);
+    $("#fah").css("border", "");
+    $("#cel").css("border", "0.05em solid white");
+    $("#cel").css("background-color", "");
+    $("#fah").css("background-color", "rgb(100, 190, 160)");
+  });
+  
+  $(document).on("click", "#fah", function(){
+    alert("clicked");
+    $("#temperature").html(res.main.temp * 9 / 5 + 32);
+    $("#cel").css("border", "");
+    $("#fah").css("border", "0.05em solid white");
+    $("#fah").css("background-color", "");
+    $("#cel").css("background-color", "rgb(100, 190, 160)");
+  });
 
   function getIcon(weather) {
     switch(weather) {
@@ -49,5 +76,5 @@ $(function() {
         return icon;
     }
   }
- 
 });
+
