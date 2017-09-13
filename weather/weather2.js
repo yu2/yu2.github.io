d9607ccc7a200256f8677a7ef0eec4349f37aebb
yu2 <tuyu0515@gmail.com>
2017-09-12 23:38:57 -0600

    Add files via upload

--- weather/weather2.js
+++ weather/weather2.js
@@ -1,56 +1,40 @@
 var url = "https://fcc-weather-api.glitch.me/api/current?";
-var res;
-var temp;
-var celsius;
+var res, temp, celsius;
 
 $(document).ready(function() {
   
   //if geolocation is available, do ajax call
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
-      lat = position.coords.latitude;
-      lon = position.coords.longitude;
+      var lat = position.coords.latitude;
+      var lon = position.coords.longitude;
       getWeather(lat, lon);
     });
   } else {
     alert("Geolocation unavailable. Please refresh page and allow location sharing.");
   }
-  
 });
 
 $(document).ajaxStop(function(){
   $("#cel").click(function(){
-    alert("hi");
     $("#temperature").text(celsius);
-    $("#fah").css("border", "");
+    $("#fah").css("border", "none");
     $("#cel").css("border", "0.05em solid white");
-    $("#cel").css("background-color", "");
+    $("#cel").css("background-color", "rgb(120, 210, 180)");
     $("#fah").css("background-color", "rgb(100, 190, 160)");
   });
   
-  //programmatically triggering works
-  //$("#cel").trigger("click");
-  
-  document.getElementById("cel").addEventListener("click", function() {
-    alert("#cel has been clicked");
-  });
-  
-  $("#fah").click(function() {
-    alert("clicked");
-  });
-  
-  $(document).on("click", "#fah", function(){
-    alert("clicked");
-    $("#temperature").text(cel * 9 / 5 + 32);
-    $("#cel").css("border", "");
+  $("#fah").click(function(){
+    $("#temperature").text(Math.round(celsius * 9 / 5 + 32));
+    $("#cel").css("border", "none");
     $("#fah").css("border", "0.05em solid white");
-    $("#fah").css("background-color", "");
+    $("#fah").css("background-color", "rgb(120, 210, 180)");
     $("#cel").css("background-color", "rgb(100, 190, 160)");
   });
 });
 
 
-function getWeather() {
+function getWeather(lat, lon) {
   $.ajax({
     url: url + "lat=" + lat + "&lon=" + lon,
     success: function(result) {
