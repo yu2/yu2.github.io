ea316328ed9a3aa3a5a3d67e4d4defe01d722cc5
yu2 <tuyu0515@gmail.com>
2017-09-25 12:46:29 -0600

    Add files via upload

--- wiki/wiki.js
+++ wiki/wiki.js
@@ -1,24 +1,54 @@
+var query, resultsJoined;
+var results = [];
+
 $(function(){
-  $(".display").text("hi");
+  console.log(results);
+  $(".resultsHere").text("hi");
+  
+  $("#submit").click(function() {
+    query = $("#searchbox").val();
+    search();
+  });
+});
+
+function search() {
   $.ajax({
     url: "http://en.wikipedia.org/w/api.php?",
     data: {
       action: 'query',
       list: 'search',
-      srsearch: 'jacksonville',
+      srsearch: query,
       //prop: 'images',
       //imlimit: '20',
       format: 'json' //changed from 'jsonfm'
     },
     dataType: 'jsonp',
-    success: function(result) {
-      console.log("success");
-      console.log(result);
-      $(".display").text(result.query.search[0].title);
+    success: function(response) {
+      console.log(response);
+      
+      //set up array of results, display them
+      for(var i = 0; i < response.query.search.length; i++) {
+        results[i] = "<p class=\"results\">" + response.query.search[i].title + "</p></br>";
+      }
+      resultsJoined = results.join("");
+      $(".resultsHere").html(resultsJoined).hide();
+      displayResults();
     }
   });
-  
-  $("#submit").click(function() {
-    $(".display").text($("#searchbox").val());
+}
+
+function displayResults() {
+  resultsCol = document.getElementsByClassName("results");
+  $(resultsCol).hide();
+  $(".resultsHere").show();
+  $(resultsCol[0]).show(250, function(){
+    $(this).next().show(250, arguments.callee); //arguments.callee contains the currently executing function
   });
-});
+  
+  
+  /*
+  for(var i = 0; i<resultsCol.length; i++) {
+    $(resultsCol[i]).show(1000);
+  }
+  */
+}
