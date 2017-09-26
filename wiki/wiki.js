d6a92cadc32d2d80b8225d4cb1862698769b2d86
yu2 <tuyu0515@gmail.com>
2017-09-25 19:51:20 -0600

    Add files via upload

--- wiki/wiki.js
+++ wiki/wiki.js
@@ -1,5 +1,6 @@
 var query, resultsJoined;
 var results = [];
+var colors = ["snow", "honeydew", "mintcream", "aliceblue", "ghostwhite", "whitesmoke", "seashell", "beige", "azure", "ivory", "mistyrose"];
 
 $(function(){
   console.log(results);
@@ -7,6 +8,7 @@ $(function(){
   
   $("#submit").click(function() {
     query = $("#searchbox").val();
+    console.log("here");
     search();
   });
 });
@@ -28,7 +30,7 @@ function search() {
       
       //set up array of results, display them
       for(var i = 0; i < response.query.search.length; i++) {
-        results[i] = "<p class=\"results\">" + response.query.search[i].title + "</p></br>";
+        results[i] = "<p class=\"results\" style=\"border: 0.05em solid black; padding: 0.2em; background-color: " + colors[Math.floor(Math.random() * 12)] + ";\">" + "<a target=\"_blank\" href=\"https://en.wikipedia.org/wiki/" + response.query.search[i].title.replace(" ", "_") + "\">" + response.query.search[i].title + "</a></p></br>";
       }
       resultsJoined = results.join("");
       $(".resultsHere").html(resultsJoined).hide();
@@ -40,7 +42,7 @@ function search() {
 function displayResults() {
   resultsCol = document.getElementsByClassName("results");
   $(resultsCol).hide();
-  $(".resultsHere").show();
+  $(".resultsHere").css("text-align", "center").show();
   $(resultsCol[0]).show(250, function(){
     $(this).next().show(250, arguments.callee); //arguments.callee contains the currently executing function
   });
