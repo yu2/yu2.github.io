3b2925f70f76bd7894f4ea52ea221e269ca42a96
yu2 <tuyu0515@gmail.com>
2017-10-07 01:06:12 -0600

    Add files via upload

--- twitch/twitch.js
+++ twitch/twitch.js
@@ -1,33 +1,41 @@
 var url = "https://api.twitch.tv/kraken/channels/trumpsc";
+var target, index;
 
 $(function() {
-  $("#go").click(function() {
-    channelInfo($("#searchbox").val());
+  $(".go").click(function() {
+    target = event.target.id;
+    index = target.substr(target.length - 1);
+    console.log(index);
+    channelInfo($("#searchbox" + index).val(), index);
   });
-  $("#searchbox").click(function() {
-    $("#searchbox").val("");
+  $(".searchbox").click(function(event) {
+    $("#" + event.target.id).val("");
   });
-  $("#searchbox").keydown(function(e) {
+  $(".searchbox").keydown(function(e) {
     if (e.keyCode == 13) {
-      channelInfo($("#searchbox").val());
+      target = event.target.id;
+      index = target.substr(target.length -1);
+      channelInfo($("#" + target).val(), index);
     }
   });
 });
 
-function channelInfo(channelName) {
+function channelInfo(channelName, boxNumber) {
   $.ajax({
     url: 'https://api.twitch.tv/kraken/streams/' + channelName,
     headers: {"Client-ID": "fcc-yu2"},
     success: function(response){
       console.log(response);
+      var currentBox = "#channel" + boxNumber;
+      console.log(boxNumber);
       if (response.stream !== null) {
-        $("#channel1").css("background-color", "#7c7");
-        $("#channel1").html("<p><span id=\"channel1-name\">" + response.stream.channel.display_name + "</span> is playing <span id=\"channel1-game\">" + response.stream.channel.game +"</span></p>");
-        $("#channel1").animate({"opacity": "1"}, 250);
+        $(currentBox).css("background-color", "#7c7");
+        $(currentBox).html("<p><span id=\"channel1-name\">" + response.stream.channel.display_name + "</span> is playing <span id=\"channel1-game\">" + response.stream.channel.game +"</span></p>");
+        $(currentBox).animate({"opacity": "1"}, 250);
         //$("#channel1-name").html(response.stream.channel.display_name);
         //$("#channel1-game").html(response.stream.channel.game);
       } else {
-        $("#channel1").animate({"opacity": "0"}, 250);
+        $(currentBox).animate({"opacity": "0"}, 250);
       }
     }
   });
