912519285fd3c82af1895f641e2ef03aa2f1c8e7
yu2 <tuyu0515@gmail.com>
2017-11-04 00:40:20 -0600

    Add files via upload

--- portfolio/home.js
+++ portfolio/home.js
@@ -1,3 +1,19 @@
 $(function() {
-  
+  var menuWidth = $(".shelf").width() - $(".toggle-display").width();
+  var shelfHidden = false;
+  $(".toggle-display").click(function() {
+    if (shelfHidden === false) {
+      $(".shelf").animate({
+        left: "-=" + menuWidth
+      });
+      shelfHidden = true;
+    } else {
+      $(".shelf").animate({
+        left: "+=" + menuWidth
+      });
+      shelfHidden = false;
+    }
+    
+    
+  });
 });
