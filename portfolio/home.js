5028b04bce72136691c143c2d6b7e5b4e04b6265
yu2 <tuyu0515@gmail.com>
2017-11-06 03:00:17 -0600

    Add files via upload

--- portfolio/home.js
+++ portfolio/home.js
@@ -1,15 +1,26 @@
 $(function() {
-  var menuWidth = $(".shelf").width() - $(".toggle-display").width();
+  //var menuWidth = $(".shelf").width() - $(".toggle-display").width();
+  var menuWidth = $(".shelf").width();
   var shelfHidden = false;
   $(".toggle-display").click(function() {
     if (shelfHidden === false) {
       $(".shelf").animate({
-        left: "-=" + menuWidth
+        width: "0%",
+        //left: "-=" + menuWidth,
+        complete: function() {
+          alert("done");
+        }
       });
       shelfHidden = true;
+      /*
+      $('.main').css({
+        "flex-grow": "1"
+      });
+      */
     } else {
       $(".shelf").animate({
-        left: "+=" + menuWidth
+        //left: "+=" + menuWidth
+        width: "100%"
       });
       shelfHidden = false;
     }
