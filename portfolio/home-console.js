3a06eea3ab39f04f3ff1caecfa36c9cb1170405d
yu2 <tuyu0515@gmail.com>
2017-11-07 02:42:34 -0600

    Add files via upload

--- portfolio/home-console.js
+++ portfolio/home-console.js
@@ -2,25 +2,12 @@ $(function() {
   console.log("hi");
   console.log("bye");
   console.log("see you");
+  console.log("later");
 });
 
 var realConsoleLog = console.log;
     console.log = function () {
-        var message = [].join.call(arguments, " ");
-        // Display message
-        $(".console-area").append("<p>" + message + "</p>");
-        //$(".console-area p:last-child").animate({opacity: "1"}, 500);
-        $.when(test()).done(function() {
-          realConsoleLog.apply(console, arguments);
-        });
-        //realConsoleLog.apply(console, arguments);
+    var message = [].join.call(arguments, " ");
+    $(".console-area").append("<p>" + message + "</p>");
+    realConsoleLog.apply(console, arguments);
     };
-    
-    
-function test() {
-  setTimeout (function() {
-    $(".console-area p:last-child").animate({opacity: "1"}, 500);
-  }, 500);
-  
-  
-}
