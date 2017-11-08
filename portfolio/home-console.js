eafebe16748f37a679fc3c403440cfdca2bfb214
yu2 <tuyu0515@gmail.com>
2017-11-07 23:36:52 -0600

    Add files via upload

--- portfolio/home-console.js
+++ portfolio/home-console.js
@@ -3,11 +3,20 @@ $(function() {
   console.log("bye");
   console.log("see you");
   console.log("later");
+  displayMessages();
 });
 
 var realConsoleLog = console.log;
     console.log = function () {
     var message = [].join.call(arguments, " ");
-    $(".console-area").append("<p>" + message + "</p>");
+    $(".console-area").append('<p class="console-msg">' + message + '</p>');
     realConsoleLog.apply(console, arguments);
     };
+
+function displayMessages() {
+  var msgs = document.getElementsByClassName('console-msg');
+  $(msgs[0]).show(100, function() {
+    $(this).next().show(100, arguments.callee); //show is in the events cue
+  });
+
+}
