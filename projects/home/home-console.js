8a4d3fb0e0c07a184e08d3602f66e47bca23878a
yu2 <tuyu0515@gmail.com>
2017-11-09 00:56:52 -0600

    Add files via upload

--- projects/home/home-console.js
+++ projects/home/home-console.js
@@ -1,8 +1,12 @@
 $(function() {
-  //***Testing area***//
-  console.log("input text between the brackets");
+  $('#console-input').focus();
   
+  //***Testing Area***//
+  console.log("input text between the brackets");
+ 
+ 
   //***//
+  
   $('#console-input').keydown(function(key) {
     if (key.keyCode == 13) {
       key.preventDefault();
@@ -20,10 +24,16 @@ var realConsoleLog = console.log;
     realConsoleLog.apply(console, arguments);
     };
 
+// display in JSON format
+function tt(arg) {
+  $(".console-area").append('<p class="console-msg">> ' + JSON.stringify(arg) + '</p>');
+}
+
 function displayMessages() {
   var msgs = document.getElementsByClassName('console-msg');
   $(msgs[0]).show(100, function() {
+    $('.console-bg').scrollTop($('.console-bg')[0].scrollHeight);
     $(this).next().show(100, arguments.callee); //show is in the events cue
+    
   });
-
 }
