fd96c8c5b10c900ba4fb76c1a80ed2a16f61d602
yu2 <tuyu0515@gmail.com>
2017-11-20 20:28:54 -0600

    Update algorithms.js

--- projects/home/algorithms.js
+++ projects/home/algorithms.js
@@ -134,3 +134,18 @@ function smallestCommons(arr) {
   }
   return max * multiplier;
 }
+
+// Finders Keepers 
+// Create a function that looks through an array (first argument) and returns the first element in the array that passes a truth test (second argument).
+function findElement(arr, func) {
+  var num = 0;
+  
+  for (var i = 0; i < arr.length; i++) {
+    if (func(arr[i]) === true) {
+      num = arr[i];
+      return num;
+    }  
+  }
+  
+  return undefined;
+}
