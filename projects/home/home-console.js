e2cde4d005fc4748769b60c7232a5f58f7fd1df3
yu2 <tuyu0515@gmail.com>
2017-12-11 23:59:50 -0600

    Add files via upload

--- projects/home/home-console.js
+++ projects/home/home-console.js
@@ -4,6 +4,82 @@ $(function() {
   //***Testing Area***//
   //console.log("input text between the brackets");
 
+function sym(args) {
+  var arg = [...arguments];
+  var red = [];
+  
+  for (var i = 0; i < arg.length - 1; i++) {
+    var result = symDiff(arg[i], arg[i+1]);
+    tt(result);
+    red.concat(symDiff(arg[i], arg[i+1]));
+  }
+  
+  return red;
+}
+
+function symDiff(arr1, arr2) {
+  var combined = arr1.concat(arr2);
+  tt("combined: " + combined.filter(e => !(arr1.includes(e) && arr2.includes(e))));
+  return combined.filter(e => !(arr1.includes(e) && arr2.includes(e)));
+}
+
+tt(sym([1, 2, 3], [5, 2, 1, 4]));
+//tt(sym([1, 2, 5], [2, 3, 5], [3, 4, 5]));
+  /*
+  function goDeeper(arr1, arr2) {
+    return red.concat(goDeeper(arr1.filter(function(val, index, array) {
+        return index != array.length - 1;
+      }), arr1[arr1.length - 1]));
+  }
+  return red;
+}
+
+    if (arr1.length !== 1) {
+      symDiff(arr1.filter(function(val, index, array) {
+        return index != array.length - 1;
+      }), arr1[arr1.length - 1]);
+    } else {
+      for (var i = 0; i < arg.length; i++) {
+        if(arr2.includes(arr1[i]) === false) {
+          red.push(arr1[i]);
+        }
+      }
+      
+      symDiff(arr1, last);
+    }
+    
+  }
+
+  function goDeeper(array) {
+    if (array.length == 1) {
+      return array;
+    }
+    return red.concat(symDiff(array.pop(), goDeeper(array)));
+  }
+
+  return red;
+  
+  function takeOut(val, index, arr) {
+    return index !== i;
+  }
+  
+  for (var i = 0; i < arg.length; i++) {
+    var current_arg = arg[i];
+    tt("current_arg = " + current_arg);
+    var leftover = arg.filter(takeOut);
+    tt("leftover = " + leftover);
+    check_current: for (var j = 0; j < current_arg.length; j++) {
+      check_leftover: for (var k = 0; k < leftover.length; k++) {
+        if (leftover[k].includes(current_arg[j])) {
+          tt(current_arg[j]);
+          continue check_current;
+        }
+      }
+      red.push(current_arg[j]);
+    }
+  }
+  */
+
 
 
   //***//
