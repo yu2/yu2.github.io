1f28abdbe11422f544e19c857d77a655f1432f48
yu2 <tuyu0515@gmail.com>
2018-02-16 22:05:58 -0600

    Add files via upload

--- prj/ydnjs/ch2/login.js
+++ prj/ydnjs/ch2/login.js
@@ -5,10 +5,20 @@ document.addEventListener("DOMContentLoaded", function(event) {
     submitButtons[i].addEventListener("click", changeMessage);
   }
   
+  var textFields = document.querySelectorAll("input[type='text']");
+  console.log(textFields);
+  console.log(textFields[0]);
+  for (let i = 0; i < textFields.length; i++) {
+    textFields[i].addEventListener("click", function(e) {
+      console.log(textFields[i].innerHTML);
+      textFields[i].setAttribute("value", "");
+    });
+  }
+  
   function changeMessage(e) {
     var statusMessage = document.getElementsByClassName("message-display")[0];
     if(e.target.id == "signup-submit") {
-      statusMessage.innerHTML = "Please Log In";
+      statusMessage.innerHTML = "Please log in";
     } else if (e.target.id == "login-submit") {
       statusMessage.innerHTML = "Login successful!";
     }
