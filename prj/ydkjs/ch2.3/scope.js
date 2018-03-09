0e955d726586390fa35c48eb89346acb30b82199
yu2 <tuyu0515@gmail.com>
2018-03-08 21:48:22 -0600

    Add files via upload

--- prj/ydkjs/ch2.3/scope.js
+++ prj/ydkjs/ch2.3/scope.js
@@ -1,12 +1,11 @@
 var num = 2;
 $(function() {
-  
   console.log(one(3));
 });
 
 function one(a) {
-    return a + two(a + 1);
-    function two(b) {
-      return b * 2;
-    }
+  function two(b) {
+    return b * 2;
   }
+  return a + two(a + 1);
+}
