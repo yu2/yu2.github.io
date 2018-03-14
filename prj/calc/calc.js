fa8035b025955c80d552963e41da3e2cfa9a2c43
yu2 <tuyu0515@gmail.com>
2018-03-13 23:45:03 -0600

    Add files via upload

--- prj/calc/calc.js
+++ prj/calc/calc.js
@@ -1,5 +1,14 @@
 $(function() {
   var displayHeight = $(".calc-display").height();
   console.log(displayHeight);
-  //$("input").css("font-size", displayHeight);
+  $("#num-display").css("font-size", displayHeight);
+  $(".btn").click(function(e) {
+    let currentBtn = e.target.id;
+    if ($("#" + currentBtn).text() == "C") {
+      $("#num-display").text("");
+    }
+    else {
+      $("#num-display").append($("#" + currentBtn).text());
+    }
+  });
 });
