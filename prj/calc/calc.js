af79c796b758eebb0f12b43c85885bea39d71211
yu2 <tuyu0515@gmail.com>
2018-03-15 22:45:34 -0600

    Add files via upload

--- prj/calc/calc.js
+++ prj/calc/calc.js
@@ -3,11 +3,21 @@ $(function() {
   $("#num-display").css("font-size", displayHeight / 1.5);
   $(".btn").click(function(e) {
     let currentBtn = e.target.id;
+    switch ($("#" + currentBtn).text()) {
+      case "C":
+        $("#num-display").text("");
+        break;
+      
+      default:
+        $("#num-display").append($("#" + currentBtn).text());
+    }
+    /*
     if ($("#" + currentBtn).text() == "C") {
       $("#num-display").text("");
     }
     else {
       $("#num-display").append($("#" + currentBtn).text());
     }
+    */
   });
 });
