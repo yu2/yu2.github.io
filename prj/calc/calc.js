91aecfb1bc5076656a4f309929153ee234e4a011
yu2 <tuyu0515@gmail.com>
2018-03-17 13:47:19 -0600

    Add files via upload

--- prj/calc/calc.js
+++ prj/calc/calc.js
@@ -1,17 +1,27 @@
+var mode;
+var mem = 0;
+
 $(function() {
   var displayHeight = $(".calc-display").height();
   $("#num-display").css("font-size", displayHeight / 1.5);
   $(".btn").click(function(e) {
     let currentBtn = e.target.id;
-    switch ($("#" + currentBtn).text()) {
+    let btnText = $("#" + currentBtn).text();
+    
+    switch (btnText) {
       case "C":
         $("#num-display").text("");
         break;
       case "+":
-        $("#op-display").text("+");
+      case "\u2212":
+      case "\u00D7":
+      case "\u00F7":
+        $("#op-display").text(btnText);
+        mem += $("#num-display").text();
+        console.log(mem);
         break;
       default:
-        $("#num-display").append($("#" + currentBtn).text());
+        $("#num-display").append(btnText);
     }
     /*
     if ($("#" + currentBtn).text() == "C") {
