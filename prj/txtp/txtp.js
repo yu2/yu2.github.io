215c7b9fff05d13fcf827497498304cfee12dc56
yu2 <tuyu0515@gmail.com>
2018-04-06 20:28:01 -0600

    Add files via upload

--- prj/txtp/txtp.js
+++ prj/txtp/txtp.js
@@ -1,3 +1,30 @@
+var input;
+var output = [];
+
 $(function() {
   
 });
+
+function readInput() {
+  input = $("#input").val().split("\n");
+  for (let i = 0; i < input.length; i++) {
+    let lineSplit = input[i].split(" ");
+    let pron = lineSplit[0];
+    lineSplit.shift();
+    for (let j = 0; j < lineSplit.length; j++) {
+      output.push(pron + " " + lineSplit[j] + "\n");
+    }
+  }
+  
+  for (let i = 0; i < output.length; i++) {
+    $("#output").append(output[i]);
+  }
+  
+}
+
+/*
+Array.forEach(function(val, index, arr) {
+  
+});
+
+*/
