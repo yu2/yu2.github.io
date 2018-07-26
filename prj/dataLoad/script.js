d247f8feb824790f35759addf0c423ac2a90f37a
yu2 <tuyu0515@gmail.com>
2018-07-26 14:33:05 -0600

    Add files via upload

--- prj/dataLoad/script.js
+++ prj/dataLoad/script.js
@@ -17,8 +17,19 @@ function handleFiles(files) {
 	}
 	const t2 = performance.now();
 	updatePerformance(t1, t2);
+	downloadBlob(content);
 }
 
 function updatePerformance(t1, t2) {
 	timer.innerHTML = t2 - t1;
+}
+
+function downloadBlob(data) {
+	let blob = new Blob([data], {type: 'text/csv'});
+	let url = URL.createObjectURL(blob);
+	let elem = window.document.createElement('a');
+	elem.href = url;
+	elem.download = 'lemma.txt';
+	document.body.appendChild(elem);
+	alert('here');
 }
