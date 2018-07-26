5a92db57a2a9b1893fe83f8020b671633fc73ea4
yu2 <tuyu0515@gmail.com>
2018-07-26 01:40:09 -0600

    Add files via upload

--- prj/dataLoad/script.js
+++ prj/dataLoad/script.js
@@ -1,18 +1,24 @@
 document.addEventListener("DOMContentLoaded", function() {	
+	timer = document.querySelector('#timeDisplay');
 });
 
+var content = [];
 function handleFiles(files) {
+	const t1 = performance.now();
 	for (let i = 0; i < files.length; i++) {
-		let file = files[i];
-		console.log(reader.readyState);
-		reader.readAsText(file);
-		console.log(file.name);
+		(function(file) {
+			var reader = new FileReader();
+			reader.onload = function(e) {
+				let piece = e.target.result;
+				content = content.concat(piece.split('\n'));
+			}
+			reader.readAsText(file);
+		 })(files[i]);
 	}
+	const t2 = performance.now();
+	updatePerformance(t1, t2);
 }
 
-var reader = new FileReader();
-reader.onload = function(e) {
-	let content = e.target.result;
-	console.log(content);
-	reader.abort();
+function updatePerformance(t1, t2) {
+	timer.innerHTML = t2 - t1;
 }
