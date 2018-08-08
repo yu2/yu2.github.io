70810edb4aa059c3fb9fd33063a2dc66e39e91e2
yu2 <tuyu0515@gmail.com>
2018-08-07 20:12:03 -0600

    Add files via upload

--- prj/dataLoad/script.js
+++ prj/dataLoad/script.js
@@ -1,21 +1,66 @@
 document.addEventListener("DOMContentLoaded", function() {	
 	timer = document.querySelector('#timeDisplay');
-	//tLoop();
-	function tLoop() {
-		for (let i = 0; i < 1000000; i++) {
-			if (i == 999999) {
-				console.log("loop finished");
-			}
-		}
-		cont();
-	}
-	function cont() {
-		console.log("cont");
-	}
 });
 
+//var handler = doWork();
+//handler.then(moreWork());
+function moreWork() {
+	const t2 = performance.now();
+	updatePerformance(t1, t2);
+	downloadBlob(content);
+}
+
 var content = [];
-/*
+function handleLemma(files) {
+	t1 = performance.now();
+	var i = 0;
+	var target = files.length;
+	doFile(files[i]);
+
+	//Recusive function that goes through each file in succession
+	function doFile(file) {
+		let reader = new FileReader();
+		reader.onload = function(e) {
+			let piece = e.target.result;
+			var re = /\s\n/;
+			pieceArray = piece.split(re);
+			//Get rid of space at the end of each word
+			pieceArray.forEach(function(currentValue, index, array) {
+				array[index] = currentValue.trim();
+			});
+			content = content.concat(pieceArray);
+			if (i < target - 1) {
+				i++;
+				doFile(files[i]);
+			}
+			else if (i == target - 1) {
+				updatePerformance(t1)
+				downloadBlob(content);
+			}
+		};
+		reader.readAsText(file);
+	}
+
+}
+
+function downloadBlob(data) {
+	let blob = new Blob([data], {type: 'text/csv'});
+	let url = URL.createObjectURL(blob);
+	let elem = window.document.createElement('a');
+	elem.href = url;
+	elem.download = 'lemma.txt';
+	elem.innerHTML = "Download";
+	document.body.appendChild(elem);
+	console.log(content);
+}
+
+function updatePerformance(t1) {
+	const t2 = performance.now();
+	timer.innerHTML = t2 - t1;
+}
+
+
+/* Doesn't work because function finishes before loops (reading files takes too long)
 function handleFiles(files) {
 	t1 = performance.now();
 	for (let i = 0; i < files.length; i++) {
@@ -36,53 +81,4 @@ function handleFiles(files) {
 	}
 }
 */
-
-function handleFiles(files) {
-	t1 = performance.now();
-	var i = 0;
-	var target = files.length;
-	doFile(files[i]);
-	function doFile(file) {
-		let reader = new FileReader();
-		reader.onload = function(e) {
-			let piece = e.target.result;
-			pieceArray = piece.split('\n');
-			content = content.concat(pieceArray);
-			if (i < target - 1) {
-				i++;
-				doFile(files[i]);
-			}
-			else if (i == target - 1) {
-				updatePerformance(t1)
-				downloadBlob(content);
-			}
-		};
-		reader.readAsText(file);
-	}
-}
-
-//var handler = doWork();
-//handler.then(moreWork());
-function moreWork() {
-	const t2 = performance.now();
-	updatePerformance(t1, t2);
-	downloadBlob(content);
-}
-
-function updatePerformance(t1) {
-	const t2 = performance.now();
-	timer.innerHTML = t2 - t1;
-}
-
 //handler.then(downloadBlob(content));
-
-function downloadBlob(data) {
-	let blob = new Blob([data], {type: 'text/csv'});
-	let url = URL.createObjectURL(blob);
-	let elem = window.document.createElement('a');
-	elem.href = url;
-	elem.download = 'lemma.txt';
-	elem.innerHTML = "Download";
-	document.body.appendChild(elem);
-	console.log(content);
-}
