46d6441000b5ce206297d45c68b9858e7b0d038e
yu2 <tuyu0515@gmail.com>
2019-01-29 12:51:42 -0600

    Add files via upload

--- prj/gen/test.js
+++ prj/gen/test.js
@@ -5,6 +5,13 @@ document.addEventListener("DOMContentLoaded", function() {
   let x = it.next().value;
   let y = it.next(7).value;
   console.log(`${x} ${y} ${it.next().value}`);
+  
+  var it2 = step(foo);
+  it2();
+  console.log(it2());
+  
+  gimmeSomething();
+  console.log(gimmeSomething());
 });
 
 // GENERATOR
@@ -14,3 +21,28 @@ function *foo() {
   var c = 8;
   return a * c;
 }
+
+// ITERATOR HELPER
+function step(gen) {
+  var it = gen();
+  var last; //keep it in this scope
+  
+  return function () {
+    last = it.next(last).value;
+    return last;
+  };
+}
+
+var gimmeSomething = (function() {
+  var nextVal;
+  
+  return function() {
+    if (nextVal === undefined) {
+      nextVal = 1;
+    }
+    else {
+      nextVal = (3 * nextVal) + 6;
+    }
+    return nextVal;
+  };
+})();
