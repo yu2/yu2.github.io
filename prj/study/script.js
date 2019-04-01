6cdd91863124a5b04de6daa393d43024c4f9e01c
yu2 <tuyu0515@gmail.com>
2019-04-01 02:14:51 -0600

    update

--- prj/study/script.js
+++ prj/study/script.js
@@ -7,6 +7,8 @@ function updatePort() {
 	for (let i = 0; i < data.length; i++) {
 		let question = document.createElement("div");
 		let answer = document.createElement("div");
+		question.className = "cell";
+		answer.className = "cell";
 		question.innerText = data[i].q;
 		answer.innerText = data[i].a;
 		port.appendChild(question);
@@ -19,6 +21,9 @@ data = [
 {q: "sigO reasons", a: ["tech challenge"]},
 {q: "logO reasons", a: ["management?"]},
 {q: "resume highlights", a: ["thing1", "thing2"]},
-{q: "education & activities", a: ["thing1", "thing2"]}
+{q: "education & activities", a: ["thing1", "thing2"]},
+{q: "basic training location? duration?", a: ["Saint-Jean-sur-Richelieu, Quebec", "15 weeks"]},
+{q: "occupational training?", a: ["CF School of Military Intelligence, Kingston, ON", "6 months"]},
+
 ];
 
