090eeb369207502ccea45a69ca0eede9fe957abd
yu2 <tuyu0515@gmail.com>
2021-12-01 08:39:27 -0500

    added test clips

--- ball/ball.js
+++ ball/ball.js
@@ -1,7 +1,13 @@
-var img;
+var img = new Image();
+img.src = "ball_noloop.gif";
+var clip1 = new Audio("audio/bite_legs_off.mp3");
+var clip2 = new Audio("audio/hate_player_game.mp3");
+var clip3 = new Audio("audio/i_am_the_smartest.mp3");
+var clip4 = new Audio("audio/rick_and_morty_sun.mp3");
+var clip5 = new Audio("audio/white_people_problems.mp3");
+
+var empty_img = new Image();
 function goodMorning() {
-  img = new Image();
-  img.src = "ball_noloop.gif";
   var imageHolder = document.getElementsByClassName("image")[0];
   imageHolder.appendChild(img);
 }
@@ -9,4 +15,5 @@ function goodMorning() {
 function buttonClick() {
   img.src = "";
   img.src = "ball_noloop.gif";
+  clip1.play;
 }
