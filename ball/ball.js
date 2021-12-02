692f1de13a90dcfeb77bbceb8dc404280da98945
yu2 <tuyu0515@gmail.com>
2021-12-01 21:31:43 -0500

    animation and sign

--- ball/ball.js
+++ ball/ball.js
@@ -1,28 +1,37 @@
 var img = new Image();
 img.src = "ball_noloop.gif";
 var clip1 = new Audio("audio/be_quiet.mp3");
-var clip2 = new Audio("audio/bitch.mp3");
+var clip2 = new Audio("audio/christmas_christmas.mp3");
 var clip3 = new Audio("audio/clean_toilets.mp3");
-var clip4 = new Audio("audio/disqualified.mp3");
-var clip5 = new Audio("audio/fart.mp3");
-var clip6 = new Audio("audio/fart_song.mp3");
-var clip7 = new Audio("audio/glip_glops.mp3");
+var clip4 = new Audio("audio/damn_girl.mp3");
+var clip5 = new Audio("audio/fart_song.mp3");
+var clip6 = new Audio("audio/great_pain.mp3");
+var clip7 = new Audio("audio/handy_hands.mp3");
 var clip8 = new Audio("audio/i_am_summer.mp3");
-var clip9 = new Audio("audio/not_cool.mp3");
-var clip10 = new Audio("audio/ooh_yeah.mp3");
-var clip11 = new Audio("audio/pickle_rick.mp3");
-var clip12 = new Audio("audio/pluto_planet.mp3");
-var clip13 = new Audio("audio/show_me.mp3");
-var clip14 = new Audio("audio/sun.mp3");
-var clip15 = new Audio("audio/wubba_lubba.mp3");
+var clip9 = new Audio("audio/i_love_you.mp3");
+var clip10 = new Audio("audio/minecraft.mp3");
+var clip11 = new Audio("audio/not_cool.mp3");
+var clip12 = new Audio("audio/ooh_yeah.mp3");
+var clip13 = new Audio("audio/poopybutthole.mp3");
+var clip14 = new Audio("audio/show_me.mp3");
+var clip15 = new Audio("audio/student_teacher.mp3");
 var clips = [clip1, clip2, clip3, clip4, clip5, clip6, clip7, clip8, clip9,
 clip10, clip11, clip12, clip13, clip14, clip15];
 var currentTrack = 1;
 var empty_img = new Image();
 
+var tooltip = document.createElement("div");
+
 function goodMorning() {
-  var imageHolder = document.getElementsByClassName("image")[0];
+  var imageHolder = document.getElementsByClassName("image_holder")[0];
+  var tooltipHolder = document.getElementsByClassName("tooltip_holder")[0];
+  tooltip.className = "tooltip";
+  tooltip.innerHTML = "Tap me";
+  tooltipHolder.appendChild(tooltip);
   imageHolder.appendChild(img);
+
+  imageHolder.classList.add("appear");
+  tooltip.classList.add("appear");
 }
 
 function onClick() {
@@ -37,6 +46,8 @@ function onClick() {
 
   currentTrack = randomNum(clips.length);
   clips[currentTrack].play();
+
+  tooltip.classList.add("disappear");
 }
 
 function randomNum(i) {
