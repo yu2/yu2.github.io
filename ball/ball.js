7bb6cc598a5bb899aaa6b625e7ca987910fe1738
yu2 <tuyu0515@gmail.com>
2021-12-03 09:22:34 -0500

    added real audio

--- ball/ball.js
+++ ball/ball.js
@@ -1,38 +1,8 @@
 var img = new Image();
 img.src = "ball_noloop.gif";
-var rick1 = new Audio("audio/rick/be_quiet.mp3");
-var rick2 = new Audio("audio/rick/christmas_christmas.mp3");
-var rick3 = new Audio("audio/rick/clean_toilets.mp3");
-var rick4 = new Audio("audio/rick/damn_girl.mp3");
-var rick5 = new Audio("audio/rick/fart_song.mp3");
-var rick6 = new Audio("audio/rick/great_pain.mp3");
-var rick7 = new Audio("audio/rick/handy_hands.mp3");
-var rick8 = new Audio("audio/rick/i_am_summer.mp3");
-var rick9 = new Audio("audio/rick/i_love_you.mp3");
-var rick10 = new Audio("audio/rick/minecraft.mp3");
-var rick11 = new Audio("audio/rick/not_cool.mp3");
-var rick12 = new Audio("audio/rick/ooh_yeah.mp3");
-var rick13 = new Audio("audio/rick/poopybutthole.mp3");
-var rick14 = new Audio("audio/rick/show_me.mp3");
-var rick15 = new Audio("audio/rick/student_teacher.mp3");
-var anime1 = new Audio("audio/anime/anta_baka.mp3");
-var anime2 = new Audio("audio/anime/ara_ara.mp3");
-var anime3 = new Audio("audio/anime/congratulations.mp3");
-var anime4 = new Audio("audio/anime/nani.mp3");
-var anime5 = new Audio("audio/anime/nepu.mp3");
-var anime6 = new Audio("audio/anime/oniichan.mp3");
-var anime7 = new Audio("audio/anime/oniichan_oniichan.mp3");
-var anime8 = new Audio("audio/anime/scatman.mp3");
-var anime9 = new Audio("audio/anime/tuturu.mp3");
-var anime10 = new Audio("audio/anime/wow.mp3");
 
-var rick = [rick1, rick2, rick3, rick4, rick5, rick6, rick7, rick8, rick9,
-rick10, rick11, rick12, rick13, rick14, rick15];
-var anime = [anime1, anime2, anime3, anime4, anime5, anime6, anime7, anime8,
-anime9, anime10];
-var clips = rick;
+var currentClip = 0;
 var currentTrack = 1;
-var empty_img = new Image();
 
 var tooltip = document.createElement("div");
 
@@ -43,7 +13,7 @@ function goodMorning() {
   document.getElementsByClassName("content_container")[0];
 
   tooltip.className = "tooltip";
-  tooltip.innerHTML = "Tap me";
+  tooltip.innerHTML = "Tap for  &nbsp<i class='fas fa-volume-up'></i>";
   tooltipHolder.appendChild(tooltip);
   imageHolder.appendChild(img);
 
@@ -56,13 +26,13 @@ function onClick() {
   img.src = "ball_noloop.gif";
   //var sounds = document.getElementsByTagName('audio');
   //for(i=0; i < clips.length; i++) clips[i].pause();
-  if (!clips[currentTrack].paused) {
-    clips[currentTrack].pause();
-    clips[currentTrack].currentTime = 0;
+  if (!clips[currentClip][currentTrack].paused) {
+    clips[currentClip][currentTrack].pause();
+    clips[currentClip][currentTrack].currentTime = 0;
   }
 
-  currentTrack = randomNum(clips.length);
-  clips[currentTrack].play();
+  currentTrack = randomNum(clips[currentClip].length);
+  clips[currentClip][currentTrack].play();
 
   tooltip.classList.add("disappear");
 }
@@ -74,9 +44,10 @@ function randomNum(i) {
 }
 
 function switchAudio() {
-  if (clips == rick) {
-    clips = anime;
+  if (currentClip < clips.length - 1) {
+    currentClip++;
+    console.log("currentClip is now " + currentClip);
   } else {
-    clips = rick;
+    currentClip = 0;
   }
 }
---
+++ ball/sounds.js
@@ -0,0 +1,38 @@
+var sig1 = new Audio("audio/sig/Fahie.m4a");
+var sig2 = new Audio("audio/sig/future_foreseen.m4a");
+var sig3 = new Audio("audio/sig/laurels.m4a");
+var sig4 = new Audio("audio/sig/Lee.m4a");
+var sig5 = new Audio("audio/sig/PB.m4a");
+var sig6 = new Audio("audio/sig/underestimate.m4a");
+var rick1 = new Audio("audio/rick/be_quiet.mp3");
+var rick2 = new Audio("audio/rick/christmas_christmas.mp3");
+var rick3 = new Audio("audio/rick/clean_toilets.mp3");
+var rick4 = new Audio("audio/rick/damn_girl.mp3");
+var rick5 = new Audio("audio/rick/fart_song.mp3");
+var rick6 = new Audio("audio/rick/great_pain.mp3");
+var rick7 = new Audio("audio/rick/handy_hands.mp3");
+var rick8 = new Audio("audio/rick/i_am_summer.mp3");
+var rick9 = new Audio("audio/rick/i_love_you.mp3");
+var rick10 = new Audio("audio/rick/minecraft.mp3");
+var rick11 = new Audio("audio/rick/not_cool.mp3");
+var rick12 = new Audio("audio/rick/ooh_yeah.mp3");
+var rick13 = new Audio("audio/rick/poopybutthole.mp3");
+var rick14 = new Audio("audio/rick/show_me.mp3");
+var rick15 = new Audio("audio/rick/student_teacher.mp3");
+var anime1 = new Audio("audio/anime/anta_baka.mp3");
+var anime2 = new Audio("audio/anime/ara_ara.mp3");
+var anime3 = new Audio("audio/anime/congratulations.mp3");
+var anime4 = new Audio("audio/anime/nani.mp3");
+var anime5 = new Audio("audio/anime/nepu.mp3");
+var anime6 = new Audio("audio/anime/oniichan.mp3");
+var anime7 = new Audio("audio/anime/oniichan_oniichan.mp3");
+var anime8 = new Audio("audio/anime/scatman.mp3");
+var anime9 = new Audio("audio/anime/tuturu.mp3");
+var anime10 = new Audio("audio/anime/wow.mp3");
+
+var rick = [rick1, rick2, rick3, rick4, rick5, rick6, rick7, rick8, rick9,
+rick10, rick11, rick12, rick13, rick14, rick15];
+var anime = [anime1, anime2, anime3, anime4, anime5, anime6, anime7, anime8,
+anime9, anime10];
+var sig = [sig1, sig2, sig3, sig4, sig4, sig5, sig6];
+var clips = [sig, rick, anime];
