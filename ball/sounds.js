113a55a75664a9cd8422490d768b7281c842e88b
yu2 <tuyu0515@gmail.com>
2021-12-11 09:41:43 -0500

    new voices

--- ball/sounds.js
+++ ball/sounds.js
@@ -2,12 +2,14 @@ var sig1 = new Audio("audio/sig/ack.mp4");
 var sig2 = new Audio("audio/sig/bullshitter.mp4");
 var sig3 = new Audio("audio/sig/disrespect.mp4");
 var sig4 = new Audio("audio/sig/dollar.mp4");
-var sig5 = new Audio("audio/sig/future_foreseen.m4a");
-var sig6 = new Audio("audio/sig/jimped.m4a");
-var sig7 = new Audio("audio/sig/kms_orion.mp4");
-var sig8 = new Audio("audio/sig/laurels.m4a");
-var sig9 = new Audio("audio/sig/sheebar.mp4");
-var sig10 = new Audio("audio/sig/underestimate.m4a");
+var sig5 = new Audio("audio/sig/four_syndicate.mp4");
+var sig6 = new Audio("audio/sig/future_foreseen.m4a");
+var sig7 = new Audio("audio/sig/jimped.m4a");
+var sig8 = new Audio("audio/sig/kms_orion.mp4");
+var sig9 = new Audio("audio/sig/laurels.m4a");
+var sig10 = new Audio("audio/sig/questions.mp4");
+var sig11 = new Audio("audio/sig/sheebar.mp4");
+var sig12 = new Audio("audio/sig/underestimate.m4a");
 var rick1 = new Audio("audio/rick/be_quiet.mp3");
 var rick2 = new Audio("audio/rick/christmas_christmas.mp3");
 var rick3 = new Audio("audio/rick/clean_toilets.mp3");
@@ -38,5 +40,6 @@ var rick = [rick1, rick2, rick3, rick4, rick5, rick6, rick7, rick8, rick9,
 rick10, rick11, rick12, rick13, rick14, rick15];
 var anime = [anime1, anime2, anime3, anime4, anime5, anime6, anime7, anime8,
 anime9, anime10];
-var sig = [sig1, sig2, sig3, sig4, sig5, sig6, sig7, sig8, sig9, sig10];
+var sig = [sig1, sig2, sig3, sig4, sig5, sig6, sig7, sig8, sig9, sig10, sig11,
+sig12];
 var playlist = [sig, rick, anime];
